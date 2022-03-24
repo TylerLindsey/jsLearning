6cb3677fb0f5106ed312008177b368398d648554
tygoli <tylerlindsey403@gmail.com>
2022-03-24 02:30:16 -0600

    added for loop to 2 arrays

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
-var userAge = 19;
-var is_subscribed = true;
+var userAge = [11, 12, 13, 14, 15, 21, 22, 23, 24, 25];
+var is_subscribed = [true, false, true, false, true, false, true, false, true, false];
+// Above here are 2 simple arrays.
 
-console.log(userAge);
-if((userAge <18) && (is_subscribed === true)) {
-  console.log('the user is under the age of 18 and is subscribed');
-} else if ((userAge >=18) && (is_subscribed == true)) {
-  console.log('the user is 18 or older and is subscribed');
-} else if ((userAge <18) && (is_subscribed == false)) {
-  console.log('the user is under the age of 18 and is not subscried');
-} else {
-// ((userAge >=18) && (is_subscribed = false)) 
-  console.log('the user is 18 or older and is not subscribed');
+for (let index=0; index < userAge.length; index++){
+  console.log(`${userAge[index]} is ${is_subscribed[index]}`);
 }
+// The for loop starts at postion 0 (the first on 0-index); then then index will go as long as the array is; and index++ makes it go one at a time. 
+// The console logs the values of the arrays together, the whole thing is in a string that prints the user age and a true/false value is they are or are not subscribed, by simply displaying the user age value and the subscription value with the word 'is' between them. 
